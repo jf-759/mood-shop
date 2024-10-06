219d4f120a9f5c6daaacccb5e1a8f2ac048352d7
jf-759 <jane.fernandez@students.dominican.edu>
2024-10-06 13:28:53 -0700

    attempt at Append to Container

---
+++ scripts.js
@@ -0,0 +1,44 @@
+import data from './data.js'
+
+const itemsContainer = document.querySelector('items')
+
+// the length of our data determines how many times this loop is run
+for (let i = 0; i < data.length; i += 1 ) {
+    // create a new div element and give it a class name
+    const newDiv = document.createElement('div');
+    newDiv.className = 'item'
+    // create an image element
+    const img = document.createElement('img');
+    // goes through each image in the list using 'i' and the for loop
+    img.src = data [i].image
+    img.width = 300
+    img.height = 300
+    // add the image to the div
+    newDiv.appendChild(img)
+
+
+    // put new div inside items container
+    itemsContainer.appendChild(newDiv)
+
+    // create a paragraph element
+    const desc = document.createElement('P')
+    // give the paragraph text from data
+    desc.innerText = data[i].desc
+    // append the paragraph to the div
+    newDiv.appendChild(desc)
+    // do the same for price
+    const price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+     
+
+}
+
+
+// reminder notes for a new developer:
+
+//      const newDiv = document.createElement('div') 
+//      same as <div></div> in html
+//      newDiv.className = 'item'
+//      same as <div class = 'item'></div> in html
